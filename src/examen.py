d7e070fa2cc4c13ecc6c5fa3b019a84279eee021
Pablo <you@example.com>
2025-03-12 19:57:04 +0100

    avance en funcion buscar_palabra

--- src/examen.py
+++ src/examen.py
@@ -1,13 +1,19 @@
 #EXAMEN FINAL DE PYTHON 2 EVA
-def buscar_palabra(nombres):
+def buscar_palabra(nombres,edades):
+    objetivo = input("Buscar nombre: ")
+    for nombre in nombres:
+        if objetivo == nombre:
+            printstr((objetivo) +"tiene" (edades[objetivo]+"años"))
 
-    nombres = ["Perantano","Zutano","Fulano","Mengano"]
 
-    edades = {
+nombres = ["Perantano","Zutano","Fulano","Mengano"]
+
+edades = {
     "Perantano" : int(45),
     "Zutano" : int(50),
     "Fulano" : int(18),
-    "Mengano" :int(0)
+    "Mengano" :int(0)  
+}
+
 
     
-}
