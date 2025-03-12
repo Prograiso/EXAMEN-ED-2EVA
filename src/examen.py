ff5bf904e06cdb5406fe3a0d6aa90395b266dbe8
Pablo <you@example.com>
2025-03-12 19:31:51 +0100

    creamos la lista y el diccionario

--- src/examen.py
+++ src/examen.py
@@ -1,2 +1,13 @@
 #EXAMEN FINAL DE PYTHON 2 EVA
+def buscar_palabra(nombres):
 
+    nombres = ["Perantano","Zutano","Fulano","Mengano"]
+
+    edades = {
+    "Perantano" : int(45),
+    "Zutano" : int(50),
+    "Fulano" : int(18),
+    "Mengano" :int(0)
+
+    
+}
